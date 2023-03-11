a70aa64b5042a510796c098f3afa175067b4e320
Ozan Ulutaş <oznulutas@gmail.com>
2023-03-11 15:33:33 +0300

    feat: emit new notification

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { SocketModule } from 'src/socket/socket.module';
 import { NotificationRepository } from './notification.repository';
 import { NotificationService } from './notification.service';
 
 @Module({
+  imports: [SocketModule],
   providers: [NotificationService, NotificationRepository],
   exports: [NotificationService],
 })
