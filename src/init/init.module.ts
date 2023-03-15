2cbb476100b169b30c3806c6c0e225041f1db252
Ozan Ulutaş <oznulutas@gmail.com>
2023-03-15 18:51:17 +0300

    feat: add message module to init module

--- src/init/init.module.ts
+++ src/init/init.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MessageModule } from 'src/message/message.module';
 import { UserModule } from 'src/user/user.module';
 import { InitController } from './init.controller';
 import { InitService } from './init.service';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, MessageModule],
   controllers: [InitController],
   providers: [InitService],
 })
