01e42009d0fabfc8177648a1303aba70bbc717b8
Ozan Ulutaş <oznulutas@gmail.com>
2023-04-07 23:08:11 +0300

    feat: create post

--- src/post/post.repository.ts
+++ src/post/post.repository.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { FriendshipStatus } from 'src/user/user.constants';
+import { CreatePostDto } from './dto';
 
 @Injectable()
 export class PostRepository {
   constructor(private prisma: PrismaService) {}
+
+  createPost(createPostDto: CreatePostDto) {
+    return this.prisma.post.create({
+      data: createPostDto,
+      select: { id: true },
+    });
+  }
 
   getPostsByUserId(userId: number) {
     return this.prisma.post.findMany({
@@ -33,11 +42,35 @@ export class PostRepository {
     return this.prisma.post.findMany({
       where: {
         user: {
-          followers: {
-            some: {
-              followerId: userId,
+          OR: [
+            {
+              followers: {
+                some: {
+                  followerId: userId,
+                },
+              },
             },
-          },
+            {
+              OR: [
+                {
+                  friends: {
+                    some: {
+                      friendshipStatusId: FriendshipStatus.ACCEPTED,
+                      senderId: userId,
+                    },
+                  },
+                },
+                {
+                  symmetricFriends: {
+                    some: {
+                      friendshipStatusId: FriendshipStatus.ACCEPTED,
+                      receiverId: userId,
+                    },
+                  },
+                },
+              ],
+            },
+          ],
         },
       },
       select: {
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { PostRepository } from './post.repository';
+import { CreatePostDto } from './dto';
 
 @Injectable()
 export class PostService {
   constructor(private postRespository: PostRepository) {}
+
+  createPost(createPostDto: CreatePostDto) {
+    return this.postRespository.createPost(createPostDto);
+  }
 
   getPostsByUserId(userId: number) {
     return this.postRespository.getPostsByUserId(userId);
